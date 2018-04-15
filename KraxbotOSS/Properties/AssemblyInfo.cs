5848ecbedea36a3b5bb5d9a0c9eb4fce6c51f7bc
kraxarn <emil202821@hotmail.com>
2018-04-15 17:17:47 +0200

    Use AssemblyInfo

--- KraxbotOSS/Properties/AssemblyInfo.cs
+++ KraxbotOSS/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CrowGames")]
 [assembly: AssemblyProduct("KraxbotOSS")]
-[assembly: AssemblyCopyright("Copyright © CrowGames 2017")]
+[assembly: AssemblyCopyright("Copyright © CrowGames 2017 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
