4221225a015358cc4eb886db190899834285e374
kraxarn <6675728+kraxarn@users.noreply.github.com>
2018-07-21 14:29:32 +0200

    Generic method for applying material design skin

---
+++ KraxbotOSS/Tools.cs
@@ -0,0 +1,17 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace KraxbotOSS
+{
+	public abstract class Tools
+	{
+		public static void SetMaterialSkin(MaterialForm form)
+		{
+			// Material skin
+			var skin = MaterialSkinManager.Instance;
+			skin.AddFormToManage(form);
+			skin.Theme = MaterialSkinManager.Themes.DARK;
+			skin.ColorScheme = new ColorScheme(Primary.Indigo500, Primary.Indigo700, Primary.Indigo100, Accent.Blue200, TextShade.WHITE);
+		}
+	}
+}
